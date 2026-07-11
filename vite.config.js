7175086adedf07b31a2bd87d7661fc16fbdce505
fdnjlbkkths-ship-it <fdnjlbkkths@gmail.com>
2026-07-11 19:52:16 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'menu.html'),
+        about: resolve(__dirname, 'about.html'),
+        contacts: resolve(__dirname, 'contacts.html'),
+      },
+    },
+  },
+});
